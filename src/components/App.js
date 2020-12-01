13a90606c70ba27142ae1c272861e64d7f9fb31b
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-12-01 10:15:29 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function Home() {
+  return (
+    <>
+      <div>You are home</div>
+    </>
+  );
+}
+function About() {
+  return (
+    <>
+      <div>You are on the about page</div>
+      <a href="/about">About</a>
+      <a href="/">Home</a>
+      <Route exact path="/about" component={About} />
+      <Route exact="/" component={Home} />
+      <Route exact path="*" component={() => "No match"} />
+    </>
+  );
 }
 
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+          <Route path="*" component={() => "No match"} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
